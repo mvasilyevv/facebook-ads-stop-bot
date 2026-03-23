485b5d211c92b3ad52cc6b855940dec236a7aca9
markvasilev <mark.vasilyevv@gmail.com>
2026-03-23 21:36:53 +0200

    Удалить неиспользуемые redirect-страницы и debug-хвосты

--- frontend/src/pages/DecisionsPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function DecisionsPage() {
-  return <Navigate to="/settings" replace />;
-}
--- frontend/src/pages/RulesPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function RulesPage() {
-  return <Navigate to="/settings" replace />;
-}
