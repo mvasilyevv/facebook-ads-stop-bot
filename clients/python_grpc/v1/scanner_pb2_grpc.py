7503cf0f61ee0551a7f7f69caaa99609a7f073a3
markvasilev <mark.vasilyevv@gmail.com>
2026-05-22 15:20:41 +0200

    fix(grpc): восстановить relative import в scanner_pb2_grpc.py

--- clients/python_grpc/v1/scanner_pb2_grpc.py
+++ clients/python_grpc/v1/scanner_pb2_grpc.py
@@ -2,7 +2,8 @@
 """Client and server classes corresponding to protobuf-defined services."""
 
 import grpc
-from v1 import scanner_pb2 as v1_dot_scanner__pb2
+
+from . import scanner_pb2 as v1_dot_scanner__pb2
 
 GRPC_GENERATED_VERSION = "1.80.0"
 GRPC_VERSION = grpc.__version__
